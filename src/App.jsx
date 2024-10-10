64234d905c94d16103a22d532dc581955f11dc65
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2024-10-10 11:21:00 +0330

    all todos data fetched by useQuery.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
+import { useQuery } from "@tanstack/react-query";
+import "./App.css";
 
 function App() {
+  const queryKey = ["all-todos"];
+  const queryFn = () => {
+    return fetch("https://jsonplaceholder.typicode.com/todos").then((res) =>
+      res.json()
+    );
+  };
+  const { data, isPending } = useQuery({ queryKey, queryFn });
+  console.log(data, isPending);
 
   return (
     <>
-    <h3>home page</h3>
+      <h3>home page</h3>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
