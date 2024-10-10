b6ec2787f90d125a2be49f3c6176f869aeb3737b
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2024-10-10 12:07:04 +0330

    usemutation for posting data is completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import "./App.css";
 
 function App() {
-  const queryKey = ["all-todos"];
-  const queryFn = () => {
-    return fetch("https://jsonplaceholder.typicode.com/todos").then((res) =>
-      res.json()
-    );
+  // const queryKey = ["all-todos"];
+  // const queryFn = () => {
+  //   return fetch("https://jsonplaceholder.typicode.com/todos").then((res) =>
+  //     res.json()
+  //   );
+  // };
+  // const { data, isPending, isLoading } = useQuery({ queryKey, queryFn });
+  // console.log({ data, isPending, isLoading });
+  const mutationFn = (data) => {
+    return fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }).then((response) => response.json());
   };
-  const { data, isPending } = useQuery({ queryKey, queryFn });
-  console.log(data, isPending);
+  const { mutate, isPending } = useMutation({ mutationFn });
+  console.log(isPending);
+  const clickHandler = () => {
+    const data = {
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    };
+    mutate(data, {
+      onSuccess: (data) => console.log(data),
+      onError: (error) => console.log(error),
+    });
+  };
 
   return (
     <>
       <h3>home page</h3>
+      <button onClick={clickHandler}>send</button>
     </>
   );
 }
